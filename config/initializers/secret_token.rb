cfc1f2744dac2df5260152cadf210ac3f604cb01
Marcus Hambraeus <marcus@codestudio.se>
2013-06-13 09:19:07 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rabble::Application.config.secret_token = 'cbb475f371a39de88d5e4d626cb6b9edaf35781ae6ac9866622715de0a51d6fc18ec35399ea15ca8f5fed85b8a7548e6daddfe0b2ffa445668fffc4063d5c7d2'
